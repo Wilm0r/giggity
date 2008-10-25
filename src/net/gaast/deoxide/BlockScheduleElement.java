16bb2095062950dcf1710842b339747e38434204
wilmer@gaast.net <>
2008-10-25 16:53:00 +0100

    Added scrolling to the information dialog.

--- src/net/gaast/deoxide/BlockScheduleElement.java
+++ src/net/gaast/deoxide/BlockScheduleElement.java
@@ -3,12 +3,14 @@ package net.gaast.deoxide;
 import java.text.SimpleDateFormat;
 
 import android.app.Activity;
-import android.app.Dialog;
+import android.app.AlertDialog;
 import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class BlockScheduleElement extends TextView implements OnClickListener {
@@ -21,32 +23,35 @@ public class BlockScheduleElement extends TextView implements OnClickListener {
 		setTextColor(0xFFFFFFFF);
 		setPadding(0, 3, 0, 0);
 		setTextSize(8);
-		setOnClickListener(this);
 	}
 	
 	public void setItem(ScheduleDataItem item_) {
 		item = item_;
+		setOnClickListener(this);
 	}
 	
 	public void onClick(View v) {
-		if (item != null) {
-			Dialog dlg = new Dialog(getContext());
-			LinearLayout content = new LinearLayout(getContext());
-			TextView desc = new TextView(getContext());
-			CheckBox cb = new CheckBox(getContext());
-	    	SimpleDateFormat df = new SimpleDateFormat("HH:mm");
-	    	
-			dlg.setTitle(getText());
-	    	desc.setText(df.format(item.getStartTime().getTime()) + "-" +
-	    			     df.format(item.getEndTime().getTime()) + ": " +
-	    			     item.getDescription());
-			cb.setText("Remind me");
-	
-			content.setOrientation(LinearLayout.VERTICAL);
-			content.addView(desc);
-			content.addView(cb);
-			dlg.setContentView(content);
-			dlg.show();
-		}
+    	SimpleDateFormat df = new SimpleDateFormat("HH:mm");
+
+		LinearLayout content = new LinearLayout(getContext());
+		content.setOrientation(LinearLayout.VERTICAL);
+
+		TextView desc = new TextView(getContext());
+		desc.setText(df.format(item.getStartTime().getTime()) + "-" +
+	    		     df.format(item.getEndTime().getTime()) + ": " +
+	    		     item.getDescription());
+
+		ScrollView descscr = new ScrollView(getContext());
+		descscr.addView(desc);
+		content.addView(descscr, new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT, 2));
+
+		CheckBox cb = new CheckBox(getContext());
+		cb.setText("Remind me");
+		content.addView(cb, new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT, 1));
+
+    	new AlertDialog.Builder(getContext())
+			.setTitle(getText())
+    		.setView(content)
+    		.show();
 	}
 }
--- src/net/gaast/deoxide/R.java
+++ src/net/gaast/deoxide/R.java
@@ -14,8 +14,10 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
+        public static final int CheckBox01=0x7f050003;
         public static final int LinearLayout01=0x7f050000;
-        public static final int TextView01=0x7f050001;
+        public static final int ScrollView01=0x7f050001;
+        public static final int TextView01=0x7f050002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
