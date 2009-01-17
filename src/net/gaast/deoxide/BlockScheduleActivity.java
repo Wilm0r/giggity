2f5bb851170db7ad6343649c3bf7ebb4e42e9ae8
wilmer@gaast.net <>
2009-01-17 00:03:44 +0000

    Removed bogus testing stuff, and now catching orientation changes and preventing full activity reloads when they happen.

--- src/net/gaast/deoxide/BlockScheduleActivity.java
+++ src/net/gaast/deoxide/BlockScheduleActivity.java
@@ -1,7 +1,9 @@
 package net.gaast.deoxide;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 
 public class BlockScheduleActivity extends Activity {
 	Schedule sched;
@@ -15,4 +17,11 @@ public class BlockScheduleActivity extends Activity {
 		bs = new BlockSchedule(this, sched);
 		setContentView(bs);
 	}
+    
+    public void onConfigurationChanged(Configuration newConfig) {
+    	super.onConfigurationChanged(newConfig);
+    	Log.i("BlockScheduleActivity", "Orientation changed");
+    	/* We really don't have to do anything special here. The
+    	 * layouts will take care of everything. */
+    }
 }
