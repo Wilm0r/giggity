7ccec19b2921fb2fc720d126c75845b0e46b9ffa
Wilmer van der Gaast <wilmer@gaast.net>
2011-01-30 22:12:25 +0000

    Use AlarmManager for reminders, the old approach didn't work without battery power which makes it pretty useless.

--- src/net/gaast/giggity/Reminder.java
+++ src/net/gaast/giggity/Reminder.java
@@ -2,78 +2,61 @@ package net.gaast.giggity;
 
 import java.util.ArrayList;
 
+import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.net.Uri;
-import android.os.Handler;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class Reminder extends Service {
+	public static final String ACTION = "net.gaast.giggity.ALARM";
+	
 	Giggity app;
-	Handler timer = new Handler();
-	Runnable sendReminder = new Runnable() {
-		@Override
-		public void run() {
-	    	SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(app);
-	    	int period = Integer.parseInt(pref.getString("reminder_period", "5")) * 60000;
-	    	
-	    	/* Keep it dumb and simple: Go through the list of marked events. If it's
-	    	 * long ago, drop it. If its reminder time is 30s away from now (either 
-	    	 * direction), remind the user now. If it's further away, set a timer and
-	    	 * stop looking for now. */
-			timer.removeCallbacks(this);
-			for (Schedule.Item item : new ArrayList<Schedule.Item>(app.getRemindItems())) {
-				long when = item.getStartTime().getTime() - period -
-				            System.currentTimeMillis();
-				if (when < -30000) {
-					/* Hmm, this one's in the past, so too late to remind. */
-					Log.d("reminder", "Dropping reminder for " + item.getTitle());
-					app.getRemindItems().remove(item);
-				} else if (when > -30000 && when < 30000) {
-					Log.d("reminder", "Generating reminder for " + item.getTitle());
-					sendReminder(item);
-					app.getRemindItems().remove(item);
-				} else {
-					Log.d("reminder", "Next alarm coming up in " + when / 1000 + " seconds");
-					timer.postDelayed(this, when);
-					break;
-					/* List is sorted by time so we're done for this iteration. */
-				}
-			}
-			
-			/* Stop the service if there's nothing else left. */
-			if (app.getRemindItems().size() == 0) {
-				Log.d("reminder", "No reminders left, let's stop the nagging");
-				stopSelf();
-			}
-		}
-	};
+	
+	int notid;
 
 	/* Vibrator pattern */
 	private long[] coolsong = { 0, 90, 60, 90, 60, 0, 150, 90, 60, 0, 150, 90, 60,
                                 90, 60, 0, 150, 150, 0, 0, 150, 0, 150, 0, 150, 1000 };
+	
+	BroadcastReceiver alarmReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			Log.d("reminder", "Who disturbs my slumber?");
+			checkAlarms();
+		}
+	};
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
 		app = (Giggity) getApplication();
+		
+		/* Run our alarm loop if we receive an alarm, or if the current
+		 * time(zone) changed. In that case we may have to reschedule alarms. */
+		Log.d("reminder", "Setting receivers");
+		registerReceiver(alarmReceiver, new IntentFilter(ACTION));
+		registerReceiver(alarmReceiver, new IntentFilter(Intent.ACTION_TIME_CHANGED));
+		registerReceiver(alarmReceiver, new IntentFilter(Intent.ACTION_TIMEZONE_CHANGED));
 	}
 	
 	@Override
 	public void onStart(Intent intent, int startId) {
-		Log.d("reminder", "Who disturbs my slumber?");
-		sendReminder.run();
+		/* This is run when the service is started or poked. */
+		checkAlarms();
 	}
 	
 	private void sendReminder(Schedule.Item item) {
-		
+		/* Generate a notification. */
     	NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
     	Notification not;
     	not = new Notification(R.drawable.deoxide_icon_48x48, item.getTitle(), item.getStartTime().getTime());
@@ -85,12 +68,49 @@ public class Reminder extends Service {
     	not.defaults |= Notification.DEFAULT_SOUND;
     	not.vibrate = coolsong; 
 
-    	nm.notify(1, not);
+    	nm.notify(item.hashCode() | (int) (item.getStartTime().getTime() / 1000), not);
 	}
 	
 	@Override
 	public IBinder onBind(Intent arg0) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public void checkAlarms() {
+    	SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(app);
+    	int period = Integer.parseInt(pref.getString("reminder_period", "5")) * 60000;
+    	
+    	/* Keep it dumb and simple: Go through the list of marked events. If it's
+    	 * long ago, drop it. If its reminder time is 30s away from now (either 
+    	 * direction), remind the user now. If it's further away, set a timer and
+    	 * stop looking for now. */
+		for (Schedule.Item item : new ArrayList<Schedule.Item>(app.getRemindItems())) {
+			long when = item.getStartTime().getTime() - period -
+			            System.currentTimeMillis();
+			if (when < -30000) {
+				/* Hmm, this one's in the past, so too late to remind. */
+				Log.d("reminder", "Dropping reminder for " + item.getTitle());
+				app.getRemindItems().remove(item);
+			} else if (when > -30000 && when < 30000) {
+				Log.d("reminder", "Generating reminder for " + item.getTitle());
+				sendReminder(item);
+				app.getRemindItems().remove(item);
+			} else {
+				Log.d("reminder", "Next alarm coming up in " + when / 1000 + " seconds for " + item.getTitle());
+				AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+				am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + when,
+				       PendingIntent.getBroadcast(Reminder.this, 0, new Intent(ACTION), 0));
+				break;
+				/* List is sorted by time so we're done for this iteration. */
+			}
+		}
+		
+		/* Stop the service if there's nothing else left. */
+		if (app.getRemindItems().size() == 0) {
+			Log.d("reminder", "No reminders left, let's stop the nagging");
+			unregisterReceiver(alarmReceiver);
+			stopSelf();
+		}
 	}
 }
