936c830b18c663db5ea9c869721521cdac3122b9
Wilmer van der Gaast <wilmer@gaast.net>
2011-07-30 17:43:40 +0100

    Get descriptions from last.fm.

--- tools/dance_valley/gen-xml.py
+++ tools/dance_valley/gen-xml.py
@@ -1,14 +1,16 @@
-#!/usr/bin/python2.4
+#!/usr/bin/python
 
 import datetime
+import hashlib
+import os
 import re
+import subprocess
+import tempfile
 import time
+import urllib2
 
 from xml.etree.ElementTree import Element, SubElement, Comment, tostring
-
-html = file("times.html", "r").read()
-html = html.replace("&amp;", "&")
-html = unicode(html, "utf-8")
+from xml.sax.saxutils import unescape
 
 class GiggityObject(object):
 	ids = set()
@@ -32,13 +34,16 @@ class Schedule(GiggityObject):
 		self.id = id
 		self.title = title
 		self.tents = []
+		self.linktypes = []
 
 	def xml(self):
 		top = Element("schedule")
 		top.attrib["id"] = self.id
 		top.attrib["title"] = self.title
-		top.attrib["xmlns"] = "http://deoxide.gaast.net/#sched"
+		top.attrib["xmlns"] = "http://gaa.st/giggity#sched"
 		
+		for type in self.linktypes:
+			top.append(type.xml())
 		for tent in self.tents:
 			top.append(tent.xml())
 		
@@ -65,6 +70,7 @@ class Item(GiggityObject):
 		self.name = name
 		self.start = None
 		self.end = None
+		self.description = None
 		self.links = []
 		self.id = self.makeid(name)
 	
@@ -75,11 +81,123 @@ class Item(GiggityObject):
 		top.attrib["startTime"] = str(int(time.mktime(self.start.timetuple())))
 		top.attrib["endTime"] = str(int(time.mktime(self.end.timetuple())))
 		
+		if self.description:
+			desc = SubElement(top, "itemDescription")
+			desc.text = self.description
+		
+		for link in self.links:
+			top.append(link.xml())
+		
+		return top
+
+class Link(GiggityObject):
+	def __init__(self, url, type="www"):
+		self.url = url
+		self.type = type
+	
+	def xml(self):
+		top = Element("itemLink")
+		top.attrib["type"] = self.type
+		top.attrib["href"] = self.url
+		return top
+
+class LinkType(GiggityObject):
+	def __init__(self, id, icon):
+		self.id = id
+		self.icon = icon
+
+	def xml(self):
+		top = Element("linkType")
+		top.attrib["id"] = self.id
+		top.attrib["icon"] = self.icon
 		return top
 
 def maketime(time):
 	h, m = time.split(":")
 	return datetime.datetime(2011, 8, 6, int(h), int(m))
+
+def fetch(url):
+	hash = hashlib.md5(url).hexdigest()
+	fn = os.path.join("_cache", hash)
+	if os.path.exists(fn):
+		cont = file(fn, "r").read()
+		head, body = cont.split("\n\n", 1)
+		f_url, code = head.split("\n")[0:2]
+		if (f_url != url):
+			raise Exception("URL mismatch in cache")
+		if int(code) != 200:
+			return None
+	
+	else:
+		f = file(fn, "w")
+		f.write("%s\n" % (url,))
+		try:
+			u = urllib2.urlopen(url)
+		except urllib2.HTTPError, e:
+			f.write("%d\n\n" % (e.code,))
+			return None
+		
+		body = u.read()
+		f.write("%d\n\n%s" % (u.code, body))
+	
+	return body
+
+def dehtml(html):
+	fn = tempfile.mktemp(suffix=".html")
+	file(fn, "w").write(html)
+	w3m = subprocess.Popen(["/usr/bin/w3m", "-dump", fn], stdout=subprocess.PIPE)
+	text = unicode(w3m.stdout.read(), "utf-8")
+	w3m.communicate()
+	os.unlink(fn)
+	text = re.sub("([^\n]) *\n *([a-zA-Z0-9])", "\\1 \\2", text)
+	return text
+
+def findlinks(item):
+	ret = []
+	
+	httpname = item.name.replace(" ", "+")
+	url = "http://www.last.fm/music/" + httpname.encode("utf-8")
+	html = fetch(url)
+	if html:
+		ret.append(Link(url, "lastfm"))
+	
+	ret.append(Link("http://www.google.com/search?q=%s" % httpname, "google"))
+	
+	return ret
+
+def finddesc(item):
+	desc = None
+	url = None
+	tags = []
+	
+	for link in item.links:
+		if link.type == "lastfm":
+			url = link.url
+			break
+	
+	if url:
+		html = fetch(url)
+		m = re.search("<div id=\"wikiAbstract\">(.*?)<div class=\"wikiOptions\">", html, re.S)
+		if m:
+			desc = dehtml(m.group(1))
+		else:
+			print "No decent description for %s, removing last.fm link.." % item.name
+			item.links = [link for link in item.links if link.url != url]
+		
+		m = re.search("Popular tags:(.*?)</div>", html, re.S)
+		if m:
+			for tag in re.findall("<a.*?>(.*?)</a>", m.group(1)):
+				if '<' not in tag:
+					tags.append(tag)
+			
+		if desc and tags:
+			desc += "\nTags: %s" % ", ".join(tags)
+	
+	return desc
+
+html = file("times.html", "r").read()
+html = html.replace("&amp;", "&")
+html = unicode(html, "utf-8")
 
 sched = Schedule("nl.dancevalley.2011", "Dance Valley 2011")
 for thtml in html.split("<strong>"):
@@ -93,19 +211,24 @@ for thtml in html.split("<strong>"):
 			_, start, end, name = m.groups()
 			
 			if not start or not end:
+				tent.items[-1].name += " %s," % name
 				pass
-				#print "TODO: No time info :-("
 			else:
 				item = Item(name)
 				item.start = maketime(start)
 				item.end = maketime(end)
 				
+				item.links += findlinks(item)
+				item.description = finddesc(item)
+				
 				if laststart and (start < laststart):
 					tent = Tent("%s (MC)" % lines[0])
 					sched.tents.append(tent)
+				laststart = start
 				
 				tent.items.append(item)
-				
-				laststart = start
+
+sched.linktypes.append(LinkType("google", "http://wilmer.gaa.st/deoxide/google.png"))
+sched.linktypes.append(LinkType("lastfm", "http://wilmer.gaa.st/deoxide/lastfm.png"))
 
 file("dancevalley.xml", "w").write(tostring(sched.xml()))
