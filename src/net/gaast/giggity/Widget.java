b983b085686d6861036f5d96ee1157e14db8402e
Wilmer van der Gaast <wilmer@gaast.net>
2012-01-28 23:27:53 +0000

    Widget updates: Translations, better alignment on ICS, include speaker + room info.

--- src/net/gaast/giggity/Widget.java
+++ src/net/gaast/giggity/Widget.java
@@ -28,31 +28,42 @@ public class Widget extends AppWidgetProvider {
 		updateWidget(context, awm);
 	}
 	
-	public static void updateWidget(Context context, AppWidgetManager appWidgetManager) {
-		Giggity app = (Giggity) context.getApplicationContext();
-		String title = "", time = "", url = "";
+	public static void updateWidget(Context ctx, AppWidgetManager appWidgetManager) {
+		Giggity app = (Giggity) ctx.getApplicationContext();
+		String top = "", title = "", bottom = "", url = "";
 		
 		Log.d("WIDGET", "onUpdate");
 		
 		for (Schedule.Item item : app.getRemindItems()) {
 			if (item.getStartTime().after(new Date())) {
-				Format df = new SimpleDateFormat("EEEE d MMMM HH:mm");
+				Format df;
 				
+				if ((item.getSchedule().getLastTime().getTime() -
+				     item.getSchedule().getFirstTime().getTime()) > (6 * 86400000))
+					df = new SimpleDateFormat(ctx.getResources().getString(R.string.widg_longdate));
+				else
+					/* If the event takes <6*24h, show just a weekday, no full date. */
+					df = new SimpleDateFormat(ctx.getResources().getString(R.string.widg_shortdate));
+				
+				top = df.format(item.getStartTime());
 				title = item.getTitle();
-				time = df.format(item.getStartTime());
+				if (item.getSpeakers().size() > 0)
+					bottom = item.getSpeakers().get(0) + " " + ctx.getResources().getString(R.string.widg_in_room) + " ";
+				bottom += item.getLine().getTitle();
 				url = item.getUrl();
 				break;
 			}
 		}
 		
-		if (title == "") {
+		if (title.equals("")) {
 			Db.Connection db = app.getDb();
 			AbstractList<Db.DbSchedule> scheds = db.getScheduleList();
 			
 			for (Db.DbSchedule sched : scheds) {
 				if (sched.getStart().after(new Date())) {
+					top = ctx.getResources().getString(R.string.widg_soon);
 					title = sched.getTitle();
-					time = Giggity.dateRange(sched.getStart(), sched.getEnd());
+					bottom = Giggity.dateRange(sched.getStart(), sched.getEnd());
 					url = sched.getUrl();
 					break;
 				}
@@ -60,18 +71,23 @@ public class Widget extends AppWidgetProvider {
 			db.sleep();
 		}
 		
-		ComponentName thisWidget = new ComponentName(context, Widget.class);
+		if (title.equals("")) {
+			title = "No upcoming events :-(";
+		}
+		
+		ComponentName thisWidget = new ComponentName(ctx, Widget.class);
 		int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
 		for (int widgetId : allWidgetIds) {
-			RemoteViews v = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+			RemoteViews v = new RemoteViews(ctx.getPackageName(), R.layout.widget_layout);
 			
 			Intent evi = new Intent(Intent.ACTION_VIEW, Uri.parse(url), app, ScheduleViewActivity.class);
 			evi.putExtra("PREFER_CACHED", true);
 			PendingIntent pi = PendingIntent.getActivity(app, 0, evi, 0);
 			v.setOnClickPendingIntent(R.id.title, pi);
 
+			v.setTextViewText(R.id.top, top);
 			v.setTextViewText(R.id.title, title);
-			v.setTextViewText(R.id.time, time);
+			v.setTextViewText(R.id.bottom, bottom);
 			appWidgetManager.updateAppWidget(widgetId, v);
 		}
 	}
