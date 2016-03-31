73abcd74f0eed394f0a59e923e757331548a2123
Wilmer van der Gaast <wilmer@gaast.net>
2016-04-01 00:03:24 +0100

    Finish Fetcher refactor: Allow using it as a non-text streamer, so I can eventually try again to fetch images/etc instead of just XML/JSON.

--- app/src/main/java/net/gaast/giggity/Fetcher.java
+++ app/src/main/java/net/gaast/giggity/Fetcher.java
@@ -11,7 +11,6 @@ import android.util.Log;
 import org.apache.commons.io.input.TeeInputStream;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -19,9 +18,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Reader;
-import java.io.Writer;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
@@ -34,11 +31,13 @@ public class Fetcher {
 	private Giggity app;
 	private File fn, fntmp = null;
 	private URLConnection dlc;
-	private BufferedReader in;
 	private Source source;
 	private Handler progressHandler;
 	private long flen;
-	
+
+	private InputStream inStream;
+	private BufferedReader inReader;
+
 	public enum Source {
 		CACHE,			/* Get from cache or fail. */
 		CACHE_ONLINE,	/* Get from cache, allow fetch if not available. */
@@ -96,46 +95,46 @@ public class Fetcher {
 			int status;
 			try {
 				status = ((HttpURLConnection)dlc).getResponseCode();
-				Log.d("Fetcher", "HTTP status "+status);
+				Log.d("Fetcher", "HTTP status " + status);
 			} catch (ClassCastException e) {
 				/* Assume success if this isn't HTTP.. */
 				status = 200;
 			}
 			if (status == 200) {
-				fntmp = new File(app.getCacheDir(), "tmp." + Schedule.hashify(url));
-				OutputStream copy = new FileOutputStream(fntmp);
-				Reader rawin;
-				String enc = dlc.getContentEncoding();
-				
-				InputStream is = dlc.getInputStream();
+				inStream = dlc.getInputStream();
+
 				if (dlc.getContentLength() > -1) {
 					flen = dlc.getContentLength();
-					is = new ProgressStream(is);
-				}
-				if (enc != null && enc.contains("gzip")) {
-					is = new GZIPInputStream(is);
+					inStream = new ProgressStream(inStream);
 				}
 
-				is = new TeeInputStream(is, copy, true);
-				in = new BufferedReader(new InputStreamReader(is, "utf-8"));
+				String enc = dlc.getContentEncoding();
+				if (enc != null && enc.contains("gzip")) {
+					inStream = new GZIPInputStream(inStream);
+				}
+
+				fntmp = new File(app.getCacheDir(), "tmp." + Schedule.hashify(url));
+				OutputStream copy = new FileOutputStream(fntmp);
+				inStream = new TeeInputStream(inStream, copy, true);  // true == close copy on close
+
 				source = Source.ONLINE;
 			} else if (status == 304) {
 				Log.i("Fetcher", "HTTP 304, using cached copy");
 				source = Source.ONLINE; /* We're reading cache, but 304 means it should be equivalent. */
-				/* Just continue, in = null so we'll read from cache. */
+				/* Just continue, inStream = null so we'll read from cache. */
 			} else {
 				throw new IOException("Download error: " + dlc.getHeaderField(0));
 			}
 		}
 
-		if (in == null && fn.canRead()) {
+		if (inStream == null && fn.canRead()) {
 			/* We have no download stream and should use the cached copy (i.e. we're offline). */
 			flen = fn.length();
-			in = new BufferedReader(new InputStreamReader(new ProgressStream(new FileInputStream(fn)), "utf-8"));
+			inStream = new ProgressStream(new FileInputStream(fn));
 			dlc = null;
 			if (source != Source.ONLINE)
 				source = Source.CACHE;
-		} else if (in == null) {
+		} else if (inStream == null) {
 			throw new IOException("No network connection or cached copy available.");
 		}
 	}
@@ -143,18 +142,38 @@ public class Fetcher {
 	public void setProgressHandler(Handler handler) {
 		progressHandler = handler;
 	}
-	
+
+	// Export InputStream. Will return null if you've requested a Reader at least once.
+	public InputStream getStream() {
+		return inStream;
+	}
+
+	// Create a BufferedReader assuming charset=utf8 if necessary and return it. Streamer interface
+	// should no longer be used after that.
 	public BufferedReader getReader() {
-		return in;
+		if (inReader == null) {
+			try {
+				inReader = new BufferedReader(new InputStreamReader(inStream, "utf-8"));
+				// I don't know whether Readers take full ownership of Streamers but let's just be
+				// sure and only ever use one of the interfaces.
+				inStream = null;
+			} catch (UnsupportedEncodingException e) {
+				// If Java stops support utf-8 we're going to have a bad time.
+			}
+		}
+		return inReader;
 	}
 	
 	public String slurp() {
 		String ret = "";
 		char[] buf = new char[1024];
 		int n;
+
+		// Just to ensure inReader exists.
+		getReader();
 		
 		try {
-			while ((n = in.read(buf, 0, buf.length)) > 0)
+			while ((n = inReader.read(buf, 0, buf.length)) > 0)
 				ret += new String(buf, 0, n);
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -171,7 +190,11 @@ public class Fetcher {
 	public void keep() {
 		try {
 			/* Close it now because otherwise (I'm just guessing) we may still flush a buffer and reset mtime. */
-			in.close();
+			if (inReader != null) {
+				inReader.close();
+			} else {
+				inStream.close();
+			}
 		} catch (IOException e) {
 		}
 		
@@ -194,6 +217,7 @@ public class Fetcher {
 			fn.delete();
 	}
 
+	/** Intermediate streamer to report progress to caller (mostly for UI). */
 	private class ProgressStream extends InputStream {
 		private InputStream in;
 		private boolean waiting;
