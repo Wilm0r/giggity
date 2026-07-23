dbb8894dcd2309321b65b5d707c27ce5ec2038e2
jpantonow <jpantonow@gmail.com>
2026-07-22 23:20:45 -0300

    Fix hashCode

--- app/src/main/java/net/gaast/giggity/Schedule.java
+++ app/src/main/java/net/gaast/giggity/Schedule.java
@@ -1142,21 +1142,21 @@ public class Schedule implements Serializable {
 		}
 
 		@Override
-		public int hashCode() {
-			// No clue what the default version does but the numbers seem too low to me.
-			// I'm using this for notification + alarm IDs now so use all 32 bits.
-			// Or at least I once was, not clear whether I still do, and whether to care about the
-			// antiquated hash algorithm?
-			try {
-				MessageDigest md5 = MessageDigest.getInstance("MD5");
-				md5.update(getUrl().getBytes());
-				byte[] raw = md5.digest();
-				return ByteBuffer.wrap(raw, 0, 4).getInt();
-			} catch (NoSuchAlgorithmException e) {  // WTF no
-				e.printStackTrace();
-				return super.hashCode();
-			}
-		}
+public int hashCode() {
+    // No clue what the default version does but the numbers seem too low to me.
+    // I'm using this for notification + alarm IDs now so use all 32 bits.
+    // Or at least I once was, not clear whether I still do, and whether to care about the
+    // antiquated hash algorithm?
+    try {
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        md5.update(getUrl().getBytes());
+        byte[] raw = md5.digest();
+        return ByteBuffer.wrap(raw, 0, 4).getInt();
+    } catch (NoSuchAlgorithmException e) {  // WTF no
+        e.printStackTrace();
+        return super.hashCode();
+    }
+}
 		
 		public Schedule getSchedule() {
 			return Schedule.this;
